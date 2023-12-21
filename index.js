13083ed03e4d3b3a4a4eac8e52eb69d3e13fcb94
KenPaolaRod <kenpaorin@gmail.com>
2023-12-20 21:05:29 -0500

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan')
 const app = express();
+const cors = require('cors')
 app.use(express.json());
+app.use(cors())
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
