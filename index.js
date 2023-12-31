ad505d6955037eb56b4fba3ce28916523324266a
KenPaolaRod <kenpaorin@gmail.com>
2023-12-31 01:30:12 -0500

    working cors fixed

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+
 app.use(express.json());
 app.use(cors({
-  credentials: true,
-}))
-
-app.use(cors(corsOptions));
+  credentials: true
+}));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+app.use(cookieParser());
+
 const productRouter = require('./routes/productsRoutes');
 const userRouter = require('./routes/userRoute')
 
-app.use(cookieParser());
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 
