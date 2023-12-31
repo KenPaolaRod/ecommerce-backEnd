031c1edab0784317afdfbb5d65fb1223c2cff8ea
KenPaolaRod <kenpaorin@gmail.com>
2023-12-31 00:50:46 -0500

    adding credentials

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  credentials: true,
+}))
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
